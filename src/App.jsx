7a7a1f2c3b399746be1e7adef854b514fd7b6b23
mutahhirkhan <mutahhir199@gmail.com>
2021-04-30 12:46:53 +0500

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App </h1>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
