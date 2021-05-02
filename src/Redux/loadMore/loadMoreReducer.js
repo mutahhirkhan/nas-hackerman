c2d2064939201de71ee98bbfd656dcdf66e189b6
mutahhirkhan <mutahhir199@gmail.com>
2021-05-02 05:55:04 +0500

    wants to optimze the fetching response time ....

--- src/Redux/loadMore/loadMoreReducer.js
+++ src/Redux/loadMore/loadMoreReducer.js
@@ -3,8 +3,6 @@ var initialState = [];
  
 var loadMoreReducer = (state = initialState, actions) => {
     var {type, payload} = actions
-    // console.log(payload)
-    // console.log(type, payload)
     switch (type) {
         case LOAD_MORE_ARTICLES:
             return [...state, ...payload.threads]
--- src/utility/utility.js
+++ src/utility/utility.js
@@ -1,9 +1,9 @@
 export var fetchData = async (link) => {
   try {
-    // var promises = [fetch(String(link)).json()]
-    // var data = await Promise.all(promises)
-    var response = await fetch(String(link));
-    var data = await response.json();
+    var promises = [fetch(String(link)).json()]
+    var data = await Promise.all(promises)
+    // var response = await fetch(String(link));
+    // var data = await response.json();
 
     return data;
   } catch (error) {
