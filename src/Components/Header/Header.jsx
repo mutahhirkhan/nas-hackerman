9889a8e64e736f18d0e23e259f0438fc3f57da40
mutahhirkhan <mutahhir199@gmail.com>
2021-05-04 16:03:24 +0500

    checking null issue....

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,13 +3,15 @@ import "./Header.css";
 
 const Header = ({ changeColor = false, padding = 0, style = {} }) => {
   return (
-    <div style={{ ...style, padding: `${padding}`,  width: "100%",
+    <div style={{ ...style, 
+    padding: `${padding}`,  
+    width: "100%",
     fontFamily: `${"var(--font-family)"}`,
     fontWeight: `${"var(--extra-bold)"}`,
     backgroundColor: "white",
     flexFlow: "initial",
     fontSize: "1.5rem",
-    boxShadow: "0 5px 5px rgba(0,0,0,0.1)" }} id="topHeader" className="center">
+    boxShadow: "0 5px 5px rgba(0,0,0,0.1)" }} className="center">
       {changeColor ? <span>HACKER</span> : <>HACKER</>}
       NEWS
       {changeColor ? <span>.</span> : <>.</>}
