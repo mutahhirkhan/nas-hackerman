3c486f1cc4a74037b5cca9a779502fea9c54de71
mutahhirkhan <mutahhir199@gmail.com>
2021-05-04 14:38:37 +0500

    .header -> .topHeader

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 
 const Header = ({ changeColor = false, padding = 0, style = {} }) => {
   return (
-    <div style={{ ...style, padding: `${padding}` }} className="header center">
+    <div style={{ ...style, padding: `${padding}` }} className="topHeader center">
       {changeColor ? <span>HACKER</span> : <>HACKER</>}
       NEWS
       {changeColor ? <span>.</span> : <>.</>}
