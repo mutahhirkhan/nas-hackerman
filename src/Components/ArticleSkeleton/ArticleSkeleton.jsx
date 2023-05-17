136ca1f650f34ae12ca6f61209c3af6259528165
mutahhirkhan <mutahhir199@gmail.com>
2023-05-18 01:10:14 +0500

    ui: skeleton alignment

--- src/Components/ArticleSkeleton/ArticleSkeleton.jsx
+++ src/Components/ArticleSkeleton/ArticleSkeleton.jsx
@@ -12,9 +12,9 @@ const ArticleSkeleton = (props) => (
     foregroundColor="#b0b0b0"
     {...props}
   >
-    <rect x="127" y="88" rx="3" ry="3" width="40%" height="6" /> 
-    <rect x="4" y="5" rx="3" ry="3" width="78%" height="6" /> 
-    <rect x="4" y="24" rx="3" ry="3" width="78%" height="50" /> 
+    <rect x="127" y="88" rx="3" ry="3" width="70%" height="6" /> 
+    <rect x="4" y="5" rx="3" ry="3" width="95%" height="6" /> 
+    <rect x="4" y="24" rx="3" ry="3" width="95%" height="50" /> 
     <rect x="4" y="88" rx="3" ry="3" width="20%" height="6" /> 
     <circle cx="116" cy="91" r="5" />
   </ContentLoader>
