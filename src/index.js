08a9eeb6c3c981226eeaacd7659f096e74a628bc
mutahhirkhan <mutahhir199@gmail.com>
2023-05-17 23:07:58 +0500

    strict mode removed

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ import { Provider } from "react-redux";
 import store from "./Redux/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+		<Provider store={store}>
+			<App />
+		</Provider>,
+		document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
